f7041bd83b48fdcd4a6c9ef28c127954285b4d16
Katie Nance <katie@nance.cc>
2024-05-04 22:18:24 -0600

    Deploy commands works

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,35 +1,26 @@
-import dotenv from "dotenv";
+import { __dirname, __filename, CLIENT_ID, DISCORD_TOKEN, GUILD_ID } from "./common.js";
 import { REST, Routes } from "discord.js";
-import fs from "fs";
-import path from "path";
-
-dotenv.config();
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
-const TOKEN = process.env.TOKEN;
-const CATBOT_DIR = process.env.CATBOT_DIR;
-
-const COMMANDS_DIR = `file:\\\\${CATBOT_DIR}\\src\\commands`;
-
-console.log(__dirname);
+import { readdirSync } from "fs";
+import { join } from "path";
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = ["file:\\", __dirname, "src", "commands"].join("\\");
-console.log(foldersPath);
-const commandFolders = fs.readdirSync(foldersPath);
-console.log(commandFolders);
+const foldersPath = join(__dirname, "src", "commands");
+const commandFolders = readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  const commandsPath = join(foldersPath, folder);
+  const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = await import(filePath).then((res) => res.default);
-    if ("data" in command && "execute" in command) {
+    const filePath = join(commandsPath, file);
+    const command = await import(filePath)
+      .then((module) => module.default)
+      .catch((error) => {
+        console.error(`[ERROR] An error occurred while importing the command at ${filePath}: ${error}`);
+      });
+
+    if (!command) break;
+    else if ("data" in command && "execute" in command) {
       commands.push(command.data.toJSON());
     } else {
       console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -37,27 +28,20 @@ for (const folder of commandFolders) {
   }
 }
 
-// // Construct and prepare an instance of the REST module
-// const rest = new REST().setToken(token);
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(DISCORD_TOKEN);
 
-// // and deploy your commands!
-// (async () => {
-//   try {
-//     console.log(
-//       `Started refreshing ${commands.length} application (/) commands.`
-//     );
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-//     // The put method is used to fully refresh all commands in the guild with the current set
-//     const data = await rest.put(
-//       Routes.applicationGuildCommands(clientId, guildId),
-//       { body: commands }
-//     );
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
 
-//     console.log(
-//       `Successfully reloaded ${data.length} application (/) commands.`
-//     );
-//   } catch (error) {
-//     // And of course, make sure you catch and log any errors!
-//     console.error(error);
-//   }
-// })();
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
